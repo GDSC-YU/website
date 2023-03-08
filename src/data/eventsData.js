8b5d2a9695e4216510d2ff95a2a22f4f77d39df2
Khalil Melhem <khalil-melhem@outlook.sa>
2023-03-08 20:33:28 +0300

    updated all content

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -1,26 +1,29 @@
 export const eventsContent = [
   {
-    eventType: "WorkShop",
-    eventTitle: "Astro",
+    eventType: "Convention",
+    eventTitle: "Black Hat MEA 👾",
     eventDescription:
-      "I have no idea what to write here, but we host various workshops from time to time, competitions, and soon a hackathon! So, stay tuned.",
-    eventImage: "/content/events/test.jpeg",
+      "We had the pleasure of attending Black Hat MEA, formerly known as @HACK. A three-day cybersecurity and hacking convention, that annually takes place in Riyadh, Saudi Arabia.",
+
+    eventImage: "/content/events/Black Hat MEA.webp",
     imageAlt: "idk",
   },
   {
     eventType: "WorkShop",
-    eventTitle: "React",
+    eventTitle: "React 🧑‍💻",
     eventDescription:
-      "I have no idea what to write here, but we host various workshops from time to time, competitions, and soon a hackathon! So, stay tuned.",
-    eventImage: "/content/events/test.jpeg",
+      "We concluded a React workshop, Were we had the honor of learning from CTO Fahad Allibdi. We are grateful for this opportunity that has enhanced our skills and knowledge.",
+
+    eventImage: "/content/events/React.jpeg",
     imageAlt: "idk",
   },
   {
-    eventType: "WorkShop",
-    eventTitle: "Svelte",
+    eventType: "Hackathon",
+    eventTitle: "Coffeethon ☕",
     eventDescription:
-      "I have no idea what to write here, but we host various workshops from time to time, competitions, and soon a hackathon! So, stay tuned.",
-    eventImage: "/content/events/test.jpeg",
+      "We were delighted to host a Coffeethon intro session with Eng. Yousef Al-Faris and Mr. Al-Mohanad Al-Marawi, the CEO of Arabian Coffee Institute and founder of Coffeethons.",
+
+    eventImage: "/content/events/Coffeethon.jpeg",
     imageAlt: "idk",
   },
 ];
