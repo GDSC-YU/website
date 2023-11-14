16dfbb54594009f4b1a317d8b0175eec38020f01
khalil Melhem <me@khalil.im>
2023-11-15 01:56:04 +0300

    feat: add header & dark mode

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Menu from '~icons/ri/menu-4-fill';
+	import Close from '~icons/ri/close-fill';
+
+	import DarkToggle from './DarkToggle.svelte';
+	import navLinks from '../../data/nav';
+
+	let isMenuOpen = false;
+</script>
+
+<header class="font-bold">
+	<nav>
+		<div class="z-50 flex w-full justify-between p-5" class:fixed={isMenuOpen}>
+			<DarkToggle />
+			<button id="menu" aria-label="Toggle Menu" on:click={() => (isMenuOpen = !isMenuOpen)}>
+				<Menu
+					class={`${
+						isMenuOpen ? 'hidden' : ''
+					} h-9 w-9 transition duration-300 ease-in-out lg:hover:scale-90`}
+				/>
+				<Close
+					class={`${
+						isMenuOpen ? '' : 'hidden'
+					} h-9 w-9 transition duration-300 ease-in-out lg:hover:scale-90`}
+				/>
+			</button>
+		</div>
+
+		<div
+			class="fixed z-40 flex h-screen w-screen flex-col items-center justify-center gap-y-10 bg-slate-200 text-black dark:bg-secret dark:text-slate-100"
+			class:hidden={!isMenuOpen}
+		>
+			{#each navLinks as { name, href }}
+				<a {href} class="text-2xl transition duration-300 ease-in-out lg:hover:scale-110">
+					{name}
+				</a>
+			{/each}
+
+			<img
+				src="/images/header-guy.svg"
+				alt="Our Cute Astronaut Guy :)"
+				class="absolute bottom-0 right-0 transition duration-300 ease-in-out lg:hover:scale-110"
+			/>
+		</div>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import '../app.postcss';
+
+	import { ModeWatcher } from 'mode-watcher';
+
+	import Header from '$lib/header';
 </script>
 
+<ModeWatcher />
+
+<Header />
 <slot />
