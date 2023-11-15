50e73e2ee7b39ce64cfad368917cf4befbfb88b6
Khalil Melhem <me@khalil.im>
2023-11-15 19:27:07 +0300

    feat: add spacer & events section

---
+++ src/data/events.ts
@@ -0,0 +1,55 @@
+const eventsData = [
+	{
+		type: 'Conference',
+		title: 'DevFest Saudi 🎉',
+		description:
+			'DevFests are local tech conferences hosted by Google Developer Groups around the world. These conferences focus on Google technologies. We enjoyed listening to experts talk about our favorite technologies and connecting with developers 💡',
+		image: '/images/events/DevFest Saudi.jpeg',
+		imageAlt: 'Machine Learning in Twitter Data Session'
+	},
+	{
+		type: 'Convention',
+		title: 'Black Hat MEA 👾',
+		description:
+			'We had the pleasure of attending Black Hat MEA, a three-day cyber security and hacking convention that brought together over 30,000 attendees, 250 exhibitors, and 200 speakers from around the world 🌎',
+
+		image: '/images/events/Black Hat MEA.webp',
+		imageAlt: 'Black Hat MEA stage'
+	},
+	{
+		type: 'WorkShop',
+		title: 'React 🧑‍💻',
+		description:
+			'A React workshop, where we had the pleasure of learning from CTO Fahad Allibdi himself, and learned about the most recent React features, best practices, and how to build high-performing React applications 🚀',
+
+		image: '/images/events/React.avif',
+		imageAlt: 'Attendees of the React workshop learning about React features'
+	},
+	{
+		type: 'Hackathon',
+		title: 'Coffeethon ☕',
+		description:
+			'We hosted a Coffeethon intro session with Eng. Yousef Al-Faris and Mr. Al-Mohanad Al-Marawi, CEO of Arabian Coffee Institute and Coffeethons founder!',
+
+		image: '/images/events/Coffeethon.jpeg',
+		imageAlt: 'Red coffee beans in hand farmers - Thailand'
+	},
+	{
+		type: 'Hackathon',
+		title: 'Desert Code Oasis 🌵',
+		description:
+			"A Java Programming Challenge to put students' programming skills to the test in a thrilling and educational experience. Participants solved 20 unique challenges against the clock to emerge victorious 🏆",
+		image: '/images/events/Desert Code Oasis.webp',
+		imageAlt: 'One of the students solving a challenge in Desert Code Oasis'
+	},
+	{
+		type: 'Ceremony',
+		title: 'Student Clubs Ceremony 🏅',
+		description:
+			'The Google Developer Student Club Al Yamamah won the third place 🥉 in the best student club contest at Al Yamamah University.',
+		image: '/images/events/Student Clubs Ceremony.jpeg',
+		imageAlt: 'Yazeed Al-Khalaf (club lead) receiving the award on behalf of the club'
+	}
+];
+
+export default eventsData;
