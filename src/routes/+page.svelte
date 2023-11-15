3e9821a149022b5f2ceed2a40fc0d2e545b50ac3
khalil Melhem <khalil.i.melhem@gmail.com>
2023-11-15 21:39:26 +0300

    chore: add title & metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,17 @@
 	import Header from '$lib/header';
 	import Events from '$lib/events';
 	import Projects from '$lib/projects';
+
+	const title = 'GDSC Al-Yamamah University';
+	const description =
+		"Welcome to the official website of Al Yamamah's University Google Developer Student Club!";
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="title" content={title} />
+	<meta name="description" content={description} />
+</svelte:head>
 
 <Header />
 <Home />
