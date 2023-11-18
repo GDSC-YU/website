ba0616fcdc11f9e0ec61f98a9e9a2a536509defe
Hala Alnadeem <hala.alnadeem@hotmail.com>
2023-11-19 01:01:10 +0300

    I have added all team members to the current Team section and removed the separate team page. Now, instead of redirecting users to another page, the team button displays the remaining core team members on the same page.

--- src/lib/team/Team.svelte
+++ src/lib/team/Team.svelte
@@ -2,6 +2,10 @@
 	import Spacer from '$lib/Spacer.svelte';
 	import TeamButton from './ui/TeamButton.svelte';
 	import TeamContainer from './ui/TeamContainer.svelte';
+
+	export let showMembers = false;
+	
+	export const toggleMembers = () =>(showMembers = !showMembers);
 </script>
 
 <section id="team">
@@ -10,6 +14,19 @@
 	<!-- Content -->
 	<div class="flex flex-col items-center gap-y-10 px-2 py-5 md:gap-y-12 lg:px-5">
 		<TeamContainer team="" />
-		<TeamButton />
+		{#if showMembers}
+			<TeamContainer team="Tech Team 💻" />
+			<TeamContainer team="Art Department 🎨" />
+			<TeamContainer team="Communications and PR 📧" />
+			<TeamContainer team="Content Creators 🤳" />
+		{/if}
+		<!-- <TeamButton /> -->
+		<div>
+			<button
+				class="transform rounded-lg border border-google-yellow px-4 py-2 text-lg font-semibold transition duration-200 ease-in hover:-translate-y-1 hover:border-transparent hover:bg-google-yellow active:translate-y-0 md:text-xl"
+				on:click={toggleMembers}
+				>{(showMembers)? "!Show The Rest" : "Show The Rest"} 😎
+			</button>
+		</div>
 	</div>
 </section>
--- src/lib/team/ui/TeamButton.svelte
+++ src/lib/team/ui/TeamButton.svelte
@@ -1,8 +1,11 @@
-<a href="/team" target="_blank">
-	<div>
-		<button
-			class="transform rounded-lg border border-google-yellow px-4 py-2 text-lg font-semibold transition duration-200 ease-in hover:-translate-y-1 hover:border-transparent hover:bg-google-yellow active:translate-y-0 md:text-xl"
-			>The Rest of Our Cool Team 😎
-		</button>
-	</div>
-</a>
+<script>
+	//import {toggleMembers} from "../Team.svelte";
+	//import {showMembers}  from "../Team.svelte";
+</script>
+<div>
+	<button
+		class="transform rounded-lg border border-google-yellow px-4 py-2 text-lg font-semibold transition duration-200 ease-in hover:-translate-y-1 hover:border-transparent hover:bg-google-yellow active:translate-y-0 md:text-xl"
+		on:click={toggleMembers}
+	>{(showMembers)? "!Show The Rest" : "Show The Rest"} 😎
+	</button>
+</div>
