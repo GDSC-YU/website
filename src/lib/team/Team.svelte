4edc71fd067a598a6d914e7921b6df5a703b85ef
Hala Alnadeem <hala.alnadeem@hotmail.com>
2023-11-19 16:38:33 +0300

    made the button to show/hide more/less members

--- src/lib/team/Team.svelte
+++ src/lib/team/Team.svelte
@@ -1,11 +1,11 @@
 <script>
 	import Spacer from '$lib/Spacer.svelte';
-	import TeamButton from './ui/TeamButton.svelte';
 	import TeamContainer from './ui/TeamContainer.svelte';
+	import TeamButton from './ui/TeamButton.svelte';
+	import navLinks from '$data/nav';
 
-	export let showMembers = false;
-	
-	export const toggleMembers = () =>(showMembers = !showMembers);
+	let areShown = false;
+	const toggleMembers = () => areShown = !areShown;
 </script>
 
 <section id="team">
@@ -14,19 +14,12 @@
 	<!-- Content -->
 	<div class="flex flex-col items-center gap-y-10 px-2 py-5 md:gap-y-12 lg:px-5">
 		<TeamContainer team="" />
-		{#if showMembers}
+		{#if areShown}
 			<TeamContainer team="Tech Team 💻" />
 			<TeamContainer team="Art Department 🎨" />
 			<TeamContainer team="Communications and PR 📧" />
 			<TeamContainer team="Content Creators 🤳" />
 		{/if}
-		<!-- <TeamButton /> -->
-		<div>
-			<button
-				class="transform rounded-lg border border-google-yellow px-4 py-2 text-lg font-semibold transition duration-200 ease-in hover:-translate-y-1 hover:border-transparent hover:bg-google-yellow active:translate-y-0 md:text-xl"
-				on:click={toggleMembers}
-				>{(showMembers)? "!Show The Rest" : "Show The Rest"} 😎
-			</button>
-		</div>
+		<TeamButton showMembers={areShown} onClick={toggleMembers} href={(areShown? '###' : '#team')}/>
 	</div>
 </section>
--- src/lib/team/ui/TeamButton.svelte
+++ src/lib/team/ui/TeamButton.svelte
@@ -1,11 +1,15 @@
-<script>
-	//import {toggleMembers} from "../Team.svelte";
-	//import {showMembers}  from "../Team.svelte";
+<script lang="ts">
+	export let showMembers : Boolean;
+	export let onClick: () => Boolean;
+	export let href : string;
 </script>
-<div>
-	<button
+<a href={href}>
+	<div>
+		<button
 		class="transform rounded-lg border border-google-yellow px-4 py-2 text-lg font-semibold transition duration-200 ease-in hover:-translate-y-1 hover:border-transparent hover:bg-google-yellow active:translate-y-0 md:text-xl"
-		on:click={toggleMembers}
+		on:click={onClick}
 	>{(showMembers)? "!Show The Rest" : "Show The Rest"} 😎
 	</button>
-</div>
+	</div>
+</a>
+	
