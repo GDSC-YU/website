18e6eec29d562b329410748a2fe7f1574526ac6e
Hala Alnadeem <hala.alnadeem@hotmail.com>
2023-11-24 02:51:50 +0300

    chore: create team arrays with random data

---
+++ src/data/teams/team23-24.ts
@@ -0,0 +1,218 @@
+export const leads = [
+	{
+		name: 'Yazeed Al-Khalaf24',
+		position: 'Club Lead',
+		year: '2023-2024',
+		image: '/images/humans/leads/Yazeed Al-Khalaf.jpeg',
+		social: {
+			twitter: 'https://twitter.com/yazeedalkhalaf',
+			github: 'https://github.com/YazeedAlKhalaf',
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/YazeedAlKhalaf/'
+		}
+	},
+	{
+		name: 'Dhoha Bediwi24',
+		position: 'Club Co-Lead',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: null,
+			github: 'https://github.com/dhoha999',
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/dhohabediwi/'
+		}
+	},
+	{
+		name: 'Khalil Melhem24',
+		position: 'Engineering Lead',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: 'https://x.com/iKhalilM',
+			github: 'https://github.com/g9wz',
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/khalil-melhem/'
+		}
+	},
+	{
+		name: 'Basma Al-Hijji24',
+		position: 'Public Relations Lead',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: null,
+			github: null,
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/basma-alhajji-44599b211'
+		}
+	},
+	{
+		name: 'Hala Al-Nadeem24',
+		position: 'Content Team Lead',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: 'https://twitter.com/AlnadeemHala',
+			github: 'https://github.com/Hala02N',
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/hala-a-489a46226'
+		}
+	},
+	{
+		name: 'Mishal Al-Harbi24',
+		position: 'Event Planning Lead',
+		year: '2023-2024',
+		image: '/images/humans/leads/Mishal Al-Harbi.jpg',
+		social: {
+			twitter: null,
+			github: null,
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/mishal-s-alharbi-a76809176'
+		}
+	},
+	{
+		name: 'Mohammed Al-Brahim24',
+		position: 'Club Treasurer',
+		year: '2023-2024',
+		image: '/images/humans/leads/Mohammed Al-Brahim.png',
+		social: {
+			twitter: 'https://twitter.com/moh_albrahim',
+			github: null,
+			instagram: null,
+			linkedin: 'http://linkedin.com/in/mohammed-alibrahim-358a39185'
+		}
+	}
+];
+
+export const engineering = [
+	{
+		name: 'Abdullah Meraj24',
+		position: 'Engineering Team',
+		year: '2023-2024',
+		image: '/images/humans/Abdullah Miraj.png',
+		social: {
+			twitter: null,
+			github: 'https://github.com/Abd-404',
+			instagram: null,
+			linkedin: null
+		}
+	},
+	{
+		name: 'SalahEddin Thabet24',
+		position: 'Engineering Team',
+		year: '2023-2024',
+		image: '/images/humans/SalahEddin Thabet.jpeg',
+		social: {
+			twitter: null,
+			github: 'https://github.com/SEDDINT',
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/salah-eddin-a510921b2'
+		}
+	}
+];
+
+export const design = [
+	{
+		name: 'Khaled Hazzam24',
+		position: 'Design Team',
+		year: '2023-2024',
+		image: '/images/humans/Khaled Hazzam.jpg',
+		social: {
+			twitter: null,
+			github: 'https://github.com/KadeHZ',
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/khaled-maher-hazzam-418972215/'
+		}
+	},
+	{
+		name: 'Osama Khlefawi24',
+		position: 'Design Team',
+		year: '2023-2024',
+		image: '/images/humans/Osama Khlefawi.jpg',
+		social: {
+			twitter: null,
+			github: 'https://github.com/Puppetie',
+			instagram: 'https://instagram.com/pupp3t.zip/',
+			linkedin: null
+		}
+	},
+	{
+		name: 'Ward Mousa24',
+		position: 'Design Team',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: null,
+			github: 'https://github.com/its-Ward',
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/ward-mousa-27bb8310a'
+		}
+	}
+];
+
+export const pr = [
+	{
+		name: 'Raghad Al-Saleh24',
+		position: 'Public Relations Team',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: null,
+			github: null,
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/raghad-alsaleh-059208220/'
+		}
+	},
+	{
+		name: 'Rasha Khalid24',
+		position: 'Public Relations Team',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: null,
+			github: null,
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/rasha-hamadeh-143517230'
+		}
+	},
+	{
+		name: 'Dinah AbuRahmah24',
+		position: 'Public Relations Team',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: null,
+			github: null,
+			instagram: null,
+			linkedin: 'https://www.linkedin.com/in/dinah-aburahmah-bb06ab24a'
+		}
+	}
+];
+
+export const content = [
+	{
+		name: 'Maryam Abdulaziz24',
+		position: 'Account Manager',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: null,
+			github: null,
+			instagram: null,
+			linkedin: 'https://linkedin.com/in/maryam-mostafa-abdulmoniem-076695254'
+		}
+	},
+	{
+		name: 'Maya Al-Saeed24',
+		position: 'Account Manager',
+		year: '2023-2024',
+		image: null,
+		social: {
+			twitter: null,
+			github: null,
+			instagram: null,
+			linkedin: null
+		}
+	}
+];
