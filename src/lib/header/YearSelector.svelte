0d7d6747ef67185888e73240ceeb33da2b61b242
Hala Alnadeem <hala.alnadeem@hotmail.com>
2023-11-24 11:59:48 +0300

    refactor: toggleMenu function

--- src/lib/header/YearSelector.svelte
+++ src/lib/header/YearSelector.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Book from '~icons/ri/book-3-line';
 
-	let isDisplayed = false;
+	let isDisplayed = true;
 	const toggleMenu = () => {
 		isDisplayed = !isDisplayed;
 	};
@@ -20,38 +20,37 @@
 			<Book class="h-9 w-9" />
 		</button>
 	</div>
-	{#if isDisplayed}
-		<div
-			class="right-50 absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-			role="menu"
-			aria-orientation="vertical"
-			aria-labelledby="menu-button"
-			tabindex="-1"
-		>
-			<div class="py-1" role="none">
-				<!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-				<a
-					href="#"
-					class="block px-4 py-2 text-sm text-gray-700"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-0">2024-2023</a
-				>
-				<a
-					href="#"
-					class="block px-4 py-2 text-sm text-gray-700"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-1">2022-2023</a
-				>
-				<a
-					href="#"
-					class="block px-4 py-2 text-sm text-gray-700"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-2">2021-2022</a
-				>
-			</div>
+	<div
+		class:hidden={isDisplayed}
+		class="right-50 absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+		role="menu"
+		aria-orientation="vertical"
+		aria-labelledby="menu-button"
+		tabindex="-1"
+	>
+		<div class="py-1" role="none">
+			<!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
+			<a
+				href="#"
+				class="block px-4 py-2 text-sm text-gray-700"
+				role="menuitem"
+				tabindex="-1"
+				id="menu-item-0">2024-2023</a
+			>
+			<a
+				href="#"
+				class="block px-4 py-2 text-sm text-gray-700"
+				role="menuitem"
+				tabindex="-1"
+				id="menu-item-1">2022-2023</a
+			>
+			<a
+				href="#"
+				class="block px-4 py-2 text-sm text-gray-700"
+				role="menuitem"
+				tabindex="-1"
+				id="menu-item-2">2021-2022</a
+			>
 		</div>
-	{/if}
+	</div>
 </div>
