2a9c0c835dac2b2aa165d8e547cf2ff62f395f7d
Hala Alnadeem <hala.alnadeem@hotmail.com>
2023-11-29 04:34:02 +0300

    chore: remove unnecessary test variable

--- src/lib/header/YearSelector.svelte
+++ src/lib/header/YearSelector.svelte
@@ -10,15 +10,10 @@
 	let selectedYear = (y: string) => {
 		year.set(y);
 	};
-	let currYear = '';
-	year.subscribe((value) => {
-		currYear = value;
-	});
 </script>
 
 <div class="relative inline-block text-left">
 	<div>
-		<p>{currYear}</p>
 		<button
 			type="button"
 			class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
