11079d5c0122397752fdc81a15f47c2367ce4b31
Khalil Melhem <me@khalil.im>
2023-12-03 23:20:42 +0300

    add events data

--- src/data/events/event23.ts
+++ src/data/events/event23.ts
@@ -2,20 +2,40 @@ import type { ClubEvent } from './index';
 
 const events23: ClubEvent[] = [
 	{
-		type: 'Hackathon',
-		title: 'Desert Code Oasis 🌵',
+		type: 'Workshop',
+		title: 'Git & GitHub 🐙',
+
 		description:
-			"A Java Programming Challenge to put students' programming skills to the test in a thrilling and educational experience. Participants solved 20 unique challenges against the clock to emerge victorious 🏆",
-		image: '/images/events/Desert Code Oasis.webp',
-		imageAlt: 'One of the students solving a challenge in Desert Code Oasis'
+			'A Git & GitHub Workshop, guided by Nawaf Al-Shawan. Participants gained knowledge on installing Git, navigating through its commands, and leveraging the GitHub Student Developer 📦',
+		image: '/images/events/2023 - 2024/Git & GitHub.jpg',
+		imageAlt: 'Participants attentively listening to the presenter during the workshop'
 	},
 	{
-		type: 'Ceremony',
-		title: 'Student Clubs Ceremony 🏅',
+		type: 'Tech Talk',
+		title: 'Enterprise Architecture 🏛️',
 		description:
-			'The Google Developer Student Club Al Yamamah won the third place 🥉 in the best student club contest at Al Yamamah University.',
-		image: '/images/events/Student Clubs Ceremony.jpeg',
-		imageAlt: 'Yazeed Al-Khalaf (club lead) receiving the award on behalf of the club'
+			'Maryam Al-Dhewalia, Manager at Solutions by STC, presented an introductory talk on Enterprise Architecture, exploring its principles and impact on both business and governmental advancements 🌆',
+		image: '/images/events/2023 - 2024/EA.jpeg',
+		imageAlt:
+			'Maryam Al-Dhewalia gesturing towards an Enterprise Architecture diagram during the talk'
+	},
+	{
+		type: 'Workshop',
+		title: 'Intro to Flutter 📱',
+		description:
+			'An introductory immersive Flutter workshop conducted by Yazeed AlKhalaf, "2022 - 2023" lead. Participants delved into cross-platform app development, getting to grips with the basics of Flutter and starting to build their apps 🧑‍💻',
+		image: '/images/events/2023 - 2024/Intro to Flutter.jpeg',
+		imageAlt:
+			'Yazeed AlKhalaf presenting the Flutter workshop with the Flutter mascot on the podium'
+	},
+	{
+		type: 'Bootcamp',
+		title: 'Game Camp 👾',
+		description:
+			'A comprehensive three-day game development bootcamp presented in Arabic by a team of elite Saudi game developers, where participants immersed themselves in the exciting world of game creation, gaining hands-on experience 🎮',
+		image: '/images/events/2023 - 2024/Game Camp.jpeg',
+		imageAlt:
+			'Ghada Baazim explaining how lighting and environment guide players in game design, using in-game imagery as an example'
 	}
 ];
 
