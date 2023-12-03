342568fd9b20c307720b01732cc44b52f74d4af6
Khalil Melhem <khalil.i.melhem@gmail.com>
2023-12-03 20:26:40 +0300

    refactor and bug fix TeamButton component

--- src/lib/team/ui/TeamButton.svelte
+++ src/lib/team/ui/TeamButton.svelte
@@ -7,28 +7,21 @@
 	$: currentYear = $year;
 </script>
 
-<div>
-	<button
-		class="transform rounded-lg border border-google-yellow px-4 py-2 text-lg font-semibold transition duration-200 ease-in hover:-translate-y-1 hover:border-transparent hover:bg-google-yellow active:translate-y-0 md:text-xl"
-		on:click={onClick}
-	>
+<div class="flex flex-col gap-5 md:flex-row">
+	<button on:click={onClick}>
 		{showMembers ? '!Show The Rest' : 'Show The Rest'} 😎
 	</button>
 
 	{#if currentYear !== '2022 - 2023'}
-		<button
-			class="transform rounded-lg border border-google-yellow px-4 py-2 text-lg font-semibold transition duration-200 ease-in hover:-translate-y-1 hover:border-transparent hover:bg-google-yellow active:translate-y-0 md:text-xl"
-			on:click={() => year.set('2022 - 2023')}
-		>
-			2022 - 2023 Team!
-		</button>
+		<button on:click={() => year.set('2022 - 2023')}>2022 - 2023 Team!</button>
 	{/if}
 	{#if currentYear !== '2023 - 2024'}
-		<button
-			class="transform rounded-lg border border-google-yellow px-4 py-2 text-lg font-semibold transition duration-200 ease-in hover:-translate-y-1 hover:border-transparent hover:bg-google-yellow active:translate-y-0 md:text-xl"
-			on:click={() => year.set('2023 - 2024')}
-		>
-			2023 - 2024 Team!
-		</button>
+		<button on:click={() => year.set('2023 - 2024')}>2023 - 2024 Team!</button>
 	{/if}
 </div>
+
+<style lang="postcss">
+	button {
+		@apply transform rounded-lg border border-google-yellow px-4 py-2 text-lg font-semibold transition duration-200 ease-in hover:-translate-y-1 hover:border-transparent hover:bg-google-yellow active:translate-y-0 md:text-xl;
+	}
+</style>
