236b8c73b75c34ce214f07fd94d25cd0d35e750f
Khalil Melhem <khalil.i.melhem@gmail.com>
2023-12-03 23:20:42 +0300

    add events data

--- src/data/events/event22.ts
+++ src/data/events/event22.ts
@@ -6,7 +6,7 @@ const events22: ClubEvent[] = [
 		title: 'DevFest Saudi 🎉',
 		description:
 			'DevFests are local tech conferences hosted by Google Developer Groups around the world. These conferences focus on Google technologies. We enjoyed listening to experts talk about our favorite technologies and connecting with developers 💡',
-		image: '/images/events/DevFest Saudi.jpeg',
+		image: '/images/events/2022 - 2023/DevFest Saudi.jpeg',
 		imageAlt: 'Machine Learning in Twitter Data Session'
 	},
 	{
@@ -15,16 +15,16 @@ const events22: ClubEvent[] = [
 		description:
 			'We had the pleasure of attending Black Hat MEA, a three-day cyber security and hacking convention that brought together over 30,000 attendees, 250 exhibitors, and 200 speakers from around the world 🌎',
 
-		image: '/images/events/Black Hat MEA.webp',
+		image: '/images/events/2022 - 2023/Black Hat MEA.webp',
 		imageAlt: 'Black Hat MEA stage'
 	},
 	{
-		type: 'WorkShop',
+		type: 'Workshop',
 		title: 'React 🧑‍💻',
 		description:
 			'A React workshop, where we had the pleasure of learning from CTO Fahad Allibdi himself, and learned about the most recent React features, best practices, and how to build high-performing React applications 🚀',
 
-		image: '/images/events/React.avif',
+		image: '/images/events/2022 - 2023/React.avif',
 		imageAlt: 'Attendees of the React workshop learning about React features'
 	},
 	{
@@ -33,7 +33,7 @@ const events22: ClubEvent[] = [
 		description:
 			'We hosted a Coffeethon intro session with Eng. Yousef Al-Faris and Mr. Al-Mohanad Al-Marawi, CEO of Arabian Coffee Institute and Coffeethons founder!',
 
-		image: '/images/events/Coffeethon.jpeg',
+		image: '/images/events/2022 - 2023/Coffeethon.jpeg',
 		imageAlt: 'Red coffee beans in hand farmers - Thailand'
 	},
 	{
@@ -41,7 +41,7 @@ const events22: ClubEvent[] = [
 		title: 'Desert Code Oasis 🌵',
 		description:
 			"A Java Programming Challenge to put students' programming skills to the test in a thrilling and educational experience. Participants solved 20 unique challenges against the clock to emerge victorious 🏆",
-		image: '/images/events/Desert Code Oasis.webp',
+		image: '/images/events/2022 - 2023/Desert Code Oasis.webp',
 		imageAlt: 'One of the students solving a challenge in Desert Code Oasis'
 	},
 	{
@@ -49,7 +49,7 @@ const events22: ClubEvent[] = [
 		title: 'Student Clubs Ceremony 🏅',
 		description:
 			'The Google Developer Student Club Al Yamamah won the third place 🥉 in the best student club contest at Al Yamamah University.',
-		image: '/images/events/Student Clubs Ceremony.jpeg',
+		image: '/images/events/2022 - 2023/Student Clubs Ceremony.jpeg',
 		imageAlt: 'Yazeed Al-Khalaf (club lead) receiving the award on behalf of the club'
 	}
 ];
