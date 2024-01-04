db05cabd790082919c04caf1ae6ca18eb06d4a44
Khalil Melhem <me@khalil.im>
2024-01-04 18:15:40 +0300

    refactor and enhance home links

--- src/lib/home/Home.svelte
+++ src/lib/home/Home.svelte
@@ -1,8 +1,9 @@
 <script>
+	import Yu from './ui/Yu.svelte';
+	import Google from './ui/Google.svelte';
+	import HomeButton from './ui/HomeButton.svelte';
 	import LogoModel from './ui/LogoModel.svelte';
 	import ARview from './ui/ARview.svelte';
-	import HomeButton from './ui/HomeButton.svelte';
-	import Google from './ui/Google.svelte';
 </script>
 
 <section
@@ -12,14 +13,7 @@
 	<div
 		class="flex flex-col items-center gap-y-9 text-center dark:text-white md:w-1/2 md:items-start md:gap-y-6 md:text-left lg:flex-grow"
 	>
-		<a
-			href="https://yu.edu.sa/campus/student-clubs/"
-			target="_blank"
-			rel="noreferrer"
-			aria-label="Al-Yamamah University Student Clubs"
-			class="text-xl font-semibold text-orange-400 underline-offset-8 hover:underline lg:text-3xl"
-			>Al-Yamamah University
-		</a>
+		<Yu />
 		<Google />
 		<h1 class="text-4xl font-bold lg:text-6xl">Student Club 🌟</h1>
 		<p class="text-xl font-bold lg:text-3xl">Learn, Grow, and Connect!</p>
