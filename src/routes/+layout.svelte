7bdab228ae915d8533cf1cc8c43aad9b2d3185c8
RawanMortada <98089617+RawanMortada@users.noreply.github.com>
2024-01-04 17:15:59 +0300

    📦 Open Sans Font (#14)
    Co-authored-by: Khalil Melhem <khalil.i.melhem@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,15 @@
 	import { ModeWatcher } from 'mode-watcher';
 </script>
 
+<svelte:head>
+	<link
+		rel="preload"
+		href="/fonts/OpenSans.woff2"
+		as="font"
+		type="font/woff"
+		crossOrigin="anonymous"
+	/>
+</svelte:head>
+
 <ModeWatcher />
 <slot />
