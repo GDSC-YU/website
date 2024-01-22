6d89a13084656dd9174189a1fbea3bc9d104c83a
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-01-22 22:54:22 +0300

    change switcher style

--- src/lib/header/parts/LangSwitcher.svelte
+++ src/lib/header/parts/LangSwitcher.svelte
@@ -14,7 +14,9 @@
 	}
 </script>
 
-<div class="rounded bg-black/20 p-2 px-3 text-lg font-bold shadow-lg">
+<div
+	class="rounded px-3 py-1 text-lg font-bold outline outline-2 outline-black transition-all duration-300 ease-in-out hover:scale-105 dark:outline-slate-200"
+>
 	{#if $locale === 'en'}
 		<button class="flex flex-row items-center gap-x-2" on:click={() => setLanguage('ar')}>
 			<SaudiFlag class="h-6 w-6" /><span>العربية</span>
