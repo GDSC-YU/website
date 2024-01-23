187286aa026d78fe34942f67ddcdb3a546b6c4be
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-01-23 21:28:50 +0300

    image alts event23

--- src/data/events/event23.ts
+++ src/data/events/event23.ts
@@ -6,31 +6,28 @@ const events23: ClubEvent[] = [
 		title: 'events23.gitWorkshopTitle',
 		description: 'events23.gitWorkshopDescription',
 		image: '/images/events/2023 - 2024/Git & GitHub.jpeg',
-		imageAlt: 'Participants attentively listening to the presenter during the workshop'
+		imageAlt: 'events23.gitWorkshopImageAlt'
 	},
 	{
 		type: 'Tech Talk',
 		title: 'events23.enterpriseTalkTitle',
 		description: 'events23.enterpriseTalkDescription',
 		image: '/images/events/2023 - 2024/EA.jpeg',
-		imageAlt:
-			'Maryam Al-Dhewalia gesturing towards an Enterprise Architecture diagram during the talk'
+		imageAlt: 'events23.enterpriseTalkImageAlt'
 	},
 	{
 		type: 'Workshop',
 		title: 'events23.flutterWorkshopTitle',
 		description: 'events23.flutterWorkshopDescription',
 		image: '/images/events/2023 - 2024/Intro to Flutter.jpeg',
-		imageAlt:
-			'Yazeed AlKhalaf presenting the Flutter workshop with the Flutter mascot on the podium'
+		imageAlt: 'events23.flutterWorkshopImageAlt'
 	},
 	{
 		type: 'Bootcamp',
 		title: 'events23.gameCampTitle',
 		description: 'events23.gameCampDescription',
 		image: '/images/events/2023 - 2024/Game Camp.jpeg',
-		imageAlt:
-			'Ghada Baazim explaining how lighting and environment guide players in game design, using in-game imagery as an example'
+		imageAlt: 'events23.gameCampImageAlt'
 	}
 ];
 
