b5c39b4ef800ed946b5c50ef0e3bd96f05c2664a
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-01-23 21:24:20 +0300

    image alts event22

--- src/data/events/event22.ts
+++ src/data/events/event22.ts
@@ -6,42 +6,42 @@ const events22: ClubEvent[] = [
 		title: 'events22.devfestTitle',
 		description: 'events22.devfestDescription',
 		image: '/images/events/2022 - 2023/DevFest Saudi.jpeg',
-		imageAlt: 'Machine Learning in Twitter Data Session'
+		imageAlt: 'events22.devfestImageAlt'
 	},
 	{
 		type: 'Convention',
 		title: 'events22.blackHatTitle',
 		description: 'events22.blackHatDescription',
 		image: '/images/events/2022 - 2023/Black Hat MEA.webp',
-		imageAlt: 'Black Hat MEA stage'
+		imageAlt: 'events22.blackHatImageAlt'
 	},
 	{
 		type: 'Workshop',
 		title: 'events22.reactWorkshopTitle',
 		description: 'events22.reactWorkshopDescription',
 		image: '/images/events/2022 - 2023/React.avif',
-		imageAlt: 'Attendees of the React workshop learning about React features'
+		imageAlt: 'events22.reactWorkshopImageAlt'
 	},
 	{
 		type: 'Hackathon',
 		title: 'events22.coffeethonTitle',
 		description: 'events22.coffeethonDescription',
 		image: '/images/events/2022 - 2023/Coffeethon.jpeg',
-		imageAlt: 'Red coffee beans in hand farmers - Thailand'
+		imageAlt: 'events22.coffeethonImageAlt'
 	},
 	{
 		type: 'Hackathon',
 		title: 'events22.desertCodeTitle',
 		description: 'events22.desertCodeDescription',
 		image: '/images/events/2022 - 2023/Desert Code Oasis.webp',
-		imageAlt: 'One of the students solving a challenge in Desert Code Oasis'
+		imageAlt: 'events22.desertCodeImageAlt'
 	},
 	{
 		type: 'Ceremony',
 		title: 'events22.studentCeremonyTitle',
 		description: 'events22.studentCeremonyDescription',
 		image: '/images/events/2022 - 2023/Student Clubs Ceremony.jpeg',
-		imageAlt: 'Yazeed Al-Khalaf (club lead) receiving the award on behalf of the club'
+		imageAlt: 'events22.studentCeremonyImageAlt'
 	}
 ];
 
