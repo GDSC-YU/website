ae2b75a064f325fd538de817f8c64c902db8493e
Khalil Melhem <me@khalil.im>
2024-05-22 19:31:34 +0300

    localize events types

--- src/data/events/event23.ts
+++ src/data/events/event23.ts
@@ -2,28 +2,28 @@ import type { ClubEvent } from './index';
 
 const events23: ClubEvent[] = [
 	{
-		type: 'Workshop',
+		type: 'eventsTypes.workshop',
 		title: 'events23.gitWorkshopTitle',
 		description: 'events23.gitWorkshopDescription',
 		image: '/images/events/2023 - 2024/Git & GitHub.jpeg',
 		imageAlt: 'events23.gitWorkshopImageAlt'
 	},
 	{
-		type: 'Tech Talk',
+		type: 'eventsTypes.techTalk',
 		title: 'events23.enterpriseTalkTitle',
 		description: 'events23.enterpriseTalkDescription',
 		image: '/images/events/2023 - 2024/EA.jpeg',
 		imageAlt: 'events23.enterpriseTalkImageAlt'
 	},
 	{
-		type: 'Workshop',
+		type: 'eventsTypes.workshop',
 		title: 'events23.flutterWorkshopTitle',
 		description: 'events23.flutterWorkshopDescription',
 		image: '/images/events/2023 - 2024/Intro to Flutter.jpeg',
 		imageAlt: 'events23.flutterWorkshopImageAlt'
 	},
 	{
-		type: 'Bootcamp',
+		type: 'eventsTypes.bootcamp',
 		title: 'events23.gameCampTitle',
 		description: 'events23.gameCampDescription',
 		image: '/images/events/2023 - 2024/Game Camp.jpeg',
