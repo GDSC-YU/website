577f6f1fc73255052c8d33f794917954408ca1db
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-05-22 21:15:30 +0300

    loading state & persistent locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,36 @@
-import { register, init } from 'svelte-i18n';
+import { register, init, locale } from 'svelte-i18n';
+
+import { writable } from 'svelte/store';
+
+export const isLocaleLoading = writable(true);
 
 register('en', () => import('./locales/en.json'));
 register('ar', () => import('./locales/ar.json'));
 
-init({ initialLocale: 'en', fallbackLocale: 'en' });
+const isClient = typeof window !== 'undefined';
+const savedLocale = isClient ? localStorage.getItem('locale') || 'en' : 'en';
+
+function setDocumentDirection(lang: string) {
+	if (isClient) {
+		document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
+	}
+}
+
+init({
+	initialLocale: savedLocale,
+	fallbackLocale: 'en'
+});
+
+if (isClient) {
+	setDocumentDirection(savedLocale);
+	isLocaleLoading.set(false);
+}
+
+if (isClient) {
+	locale.subscribe((currentLocale) => {
+		if (currentLocale) {
+			localStorage.setItem('locale', currentLocale);
+			setDocumentDirection(currentLocale);
+		}
+	});
+}
