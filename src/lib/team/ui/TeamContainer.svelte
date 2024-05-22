15abc77587bf644544cdd2f45ddc8a959640e1ba
Khalil Melhem <khalil.i.melhem@gmail.com>
2024-05-22 17:57:02 +0300

    pnpm prettier --write .

--- src/lib/team/ui/TeamContainer.svelte
+++ src/lib/team/ui/TeamContainer.svelte
@@ -4,59 +4,59 @@
 	import { teamData } from '$data/teams/index';
 	import { _ } from 'svelte-i18n';
 	import type { TeamMember, TeamData } from '$data/teams/index';
-  
+
 	export let team: string;
 	let selectedTeamMembers: TeamMember[];
-  
+
 	const teamKeyMapping: Record<string, string> = {
-	  'Tech Team 💻': 'Engineering',
-	  'Art Department 🎨': 'Design',
-	  'Communications and PR 📧': 'Pr',
-	  'Content Creators 🤳': 'Content'
+		'Tech Team 💻': 'Engineering',
+		'Art Department 🎨': 'Design',
+		'Communications and PR 📧': 'Pr',
+		'Content Creators 🤳': 'Content'
 	};
-  
+
 	const getTeamMembers = (year: string, team: string): TeamMember[] => {
-	  const teams: TeamData = teamData[year as keyof typeof teamData];
-  
-	  if (!teams) {
-		return [];
-	  }
-  
-	  if (team === '') {
-		return teams.Leads;
-	  }
-  
-	  const teamKeyword = teamKeyMapping[team];
-  
-	  if (teamKeyword === 'Leads') {
-		return teams.Leads;
-	  } else {
-		const relevantLeads = teams.Leads.filter((lead) => lead.team.includes(teamKeyword));
-		const teamMembers = (teams as any)[teamKeyword] || [];
-  
-		return [...relevantLeads, ...teamMembers];
-	  }
+		const teams: TeamData = teamData[year as keyof typeof teamData];
+
+		if (!teams) {
+			return [];
+		}
+
+		if (team === '') {
+			return teams.Leads;
+		}
+
+		const teamKeyword = teamKeyMapping[team];
+
+		if (teamKeyword === 'Leads') {
+			return teams.Leads;
+		} else {
+			const relevantLeads = teams.Leads.filter((lead) => lead.team.includes(teamKeyword));
+			const teamMembers = (teams as any)[teamKeyword] || [];
+
+			return [...relevantLeads, ...teamMembers];
+		}
 	};
-  
+
 	$: selectedTeamMembers = getTeamMembers($year, team);
-  </script>
-  
-  {#if team}
+</script>
+
+{#if team}
 	<h1 class="px-1 text-center text-3xl font-semibold dark:text-white md:text-5xl">
-	  {team}
+		{team}
 	</h1>
-  {/if}
-  
-  <div class="grid w-full flex-wrap justify-items-center gap-y-4 md:grid-cols-2 lg:grid-cols-3">
+{/if}
+
+<div class="grid w-full flex-wrap justify-items-center gap-y-4 md:grid-cols-2 lg:grid-cols-3">
 	{#each selectedTeamMembers as member}
-	  <TeamMemberComponent
-		img={member.image}
-		name={$_(`teamMembers.${member.nameKey}.name`)}
-		position={$_(`teamMembers.${member.positionKey}.position`)}
-		twitter={member.social.twitter}
-		github={member.social.github}
-		instagram={member.social.instagram}
-		linkedin={member.social.linkedin}
-	  />
+		<TeamMemberComponent
+			img={member.image}
+			name={$_(`teamMembers.${member.nameKey}.name`)}
+			position={$_(`teamMembers.${member.positionKey}.position`)}
+			twitter={member.social.twitter}
+			github={member.social.github}
+			instagram={member.social.instagram}
+			linkedin={member.social.linkedin}
+		/>
 	{/each}
-  </div>
+</div>
