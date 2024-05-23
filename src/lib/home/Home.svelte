172ca84009dd4006395743146c8fef4cc54343b1
Khalil Melhem <me@khalil.im>
2024-05-23 14:37:37 +0300

    enhance home page responsiveness

--- src/lib/home/Home.svelte
+++ src/lib/home/Home.svelte
@@ -10,13 +10,13 @@
 
 <section
 	id="home"
-	class="flex flex-col items-center justify-between gap-y-10 py-14 md:flex-row md:pr-4 lg:px-10 lg:py-20"
+	class="flex flex-col items-center justify-between gap-y-10 py-14 md:flex-row md:px-4 lg:px-10 lg:py-20"
 >
 	<div
-		class="flex flex-col items-center gap-y-7 text-center dark:text-white md:w-1/2 md:items-start md:gap-y-8 md:text-left lg:flex-grow"
+		class="flex flex-col items-center gap-y-5 text-center dark:text-white md:items-start lg:gap-y-10"
 	>
 		<h1
-			class="text-4xl leading-relaxed xl:text-6xl ltr:font-semibold md:ltr:text-left rtl:font-medium md:rtl:text-right"
+			class="text-4xl xl:text-6xl ltr:font-semibold md:ltr:text-left rtl:font-medium md:rtl:text-right"
 		>
 			{#if $locale === 'ar'}
 				{$t('Dev')}{' '}
@@ -30,7 +30,7 @@
 		<p class="text-xl lg:text-3xl ltr:font-semibold rtl:font-medium">{$t('LGC')}</p>
 		<HomeButton />
 	</div>
-	<div class="flex w-5/6 flex-col items-center md:w-1/2 lg:min-h-[24.5rem] lg:w-full lg:max-w-xl">
+	<div class="flex w-5/6 flex-col items-center">
 		<LogoModel />
 		<ARview />
 	</div>
