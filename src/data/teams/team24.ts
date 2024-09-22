2b47a0b78faa624250e8f8a1aa09fb977bc02e5c
Amro Elian <elianamro5@gmail.com>
2024-09-22 14:42:01 +0300

    team24 update
    I made a new team section with an empty structure and roles.

---
+++ src/data/teams/team24.ts
@@ -0,0 +1,190 @@
+import type { TeamMember } from './index';
+
+export const leads24: TeamMember[] = [
+	{
+		nameKey: '24name1',
+		positionKey: '24name1',
+		team: ['Leads'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name2',
+		positionKey: '24name2',
+		team: ['Leads'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name3',
+		positionKey: '24name3',
+		team: ['Leads', 'Pr'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name4',
+		positionKey: '24name4',
+		team: ['Leads', 'Pr'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name5',
+		positionKey: '24name5',
+		team: ['leads', 'Design'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	}
+];
+
+export const engineering24: TeamMember[] = [
+	{
+		nameKey: '24name6',
+		positionKey: '24name6',
+		team: ['Engineering'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name7',
+		positionKey: '24name7',
+		team: ['Engineering'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name8',
+		positionKey: '24name8',
+		team: ['Engineering'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name9',
+		positionKey: '24name9',
+		team: ['Engineering'],
+		social: {
+			github: '',
+			linkedin: '',
+            twitter: ''
+		}
+	},
+	{
+		nameKey: '24name10',
+		positionKey: '24name10',
+		team: ['Engineering'],
+		social: {
+            twitter: '',
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name11',
+		positionKey: '24name11',
+		team: ['Engineering'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name12',
+		positionKey: '24name12',
+		team: ['Engineering'],
+		social: {
+			linkedin: ''
+		}
+	}
+];
+
+export const design24: TeamMember[] = [
+	{
+		nameKey: '24name13',
+		positionKey: '24name13',
+		team: ['Design'],
+		social: {}
+	},
+	{
+		nameKey: '24name14',
+		positionKey: '24name14',
+		team: ['Design'],
+		social: {}
+	},
+	{
+		nameKey: '24name15',
+		positionKey: '24name15',
+		team: ['Design'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name16',
+		positionKey: '24name16',
+		team: ['Design'],
+		social: {
+			github: '',
+            instagram: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name17',
+		positionKey: '24name17',
+		team: ['Design'],
+		social: {
+			instagram: '',
+			linkedin: ''
+		}
+	}
+];
+
+export const pr24: TeamMember[] = [
+	{
+		nameKey: '24name18',
+		positionKey: '24name18',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	}
+];
+
+export const content24: TeamMember[] = [
+	{
+		nameKey: '24name19',
+		positionKey: '24name19',
+		team: ['leads', 'Content'],
+		social: {
+            twitter: '',
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name20',
+		positionKey: '24name20',
+		team: ['Content'],
+		social: {
+			twitter: ''
+		}
+	}
+];
