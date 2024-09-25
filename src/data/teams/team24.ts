6d40db92d0f2993bcb7073b2b42bc37c9000d963
01jawhara <aljawharabinaskar@gmail.com>
2024-09-25 23:24:30 +0300

    data update

--- src/data/teams/team24.ts
+++ src/data/teams/team24.ts
@@ -5,9 +5,10 @@ export const leads24: TeamMember[] = [
 		nameKey: '24name1',
 		positionKey: '24name1',
 		team: ['Leads'],
+		image: '/images/humans/Ayham K. Zeidan.png',
 		social: {
-			github: '',
-			linkedin: ''
+			github: 'https://github.com/ayhamowl',
+			linkedin: 'https://linkedin.com/in/ayham-zeidan-379053221'
 		}
 	},
 	{
@@ -15,23 +16,24 @@ export const leads24: TeamMember[] = [
 		positionKey: '24name2',
 		team: ['Leads'],
 		social: {
-			github: '',
-			linkedin: ''
+			github: 'https://github.com/Shaar03',
+			linkedin: 'https://linkedin.com/in/adnanchaar03'
 		}
 	},
 	{
 		nameKey: '24name3',
 		positionKey: '24name3',
 		team: ['Leads', 'Pr'],
+		image: '/images/humans/Raghad Al-Saleh.png',
 		social: {
-			github: '',
-			linkedin: ''
+			github: 'https://github.com/RGD111',
+			linkedin: 'https://linkedin.com/in/ward-mousa-27bb8310a'
 		}
 	},
 	{
 		nameKey: '24name4',
 		positionKey: '24name4',
-		team: ['Leads', 'Pr'],
+		team: ['Leads', 'Design'],
 		social: {
 			github: '',
 			linkedin: ''
@@ -40,7 +42,7 @@ export const leads24: TeamMember[] = [
 	{
 		nameKey: '24name5',
 		positionKey: '24name5',
-		team: ['leads', 'Design'],
+		team: ['Leads', 'Content'],
 		social: {
 			github: '',
 			linkedin: ''
@@ -61,9 +63,10 @@ export const engineering24: TeamMember[] = [
 	{
 		nameKey: '24name7',
 		positionKey: '24name7',
+		image: '/images/humans/Abdullah Miraj.png',
 		team: ['Engineering'],
 		social: {
-			github: '',
+			github: 'https://github.com/Abd-404',
 			linkedin: ''
 		}
 	},
@@ -82,8 +85,7 @@ export const engineering24: TeamMember[] = [
 		team: ['Engineering'],
 		social: {
 			github: '',
-			linkedin: '',
-            twitter: ''
+			linkedin: ''
 		}
 	},
 	{
@@ -91,7 +93,6 @@ export const engineering24: TeamMember[] = [
 		positionKey: '24name10',
 		team: ['Engineering'],
 		social: {
-            twitter: '',
 			github: '',
 			linkedin: ''
 		}
@@ -101,6 +102,7 @@ export const engineering24: TeamMember[] = [
 		positionKey: '24name11',
 		team: ['Engineering'],
 		social: {
+			github: '',
 			linkedin: ''
 		}
 	},
@@ -109,6 +111,25 @@ export const engineering24: TeamMember[] = [
 		positionKey: '24name12',
 		team: ['Engineering'],
 		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name13',
+		positionKey: '24name13',
+		team: ['Engineering'],
+		social: {
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name14',
+		positionKey: '24name14',
+		team: ['Engineering'],
+		social: {
+			github: '',
 			linkedin: ''
 		}
 	}
@@ -116,48 +137,40 @@ export const engineering24: TeamMember[] = [
 
 export const design24: TeamMember[] = [
 	{
-		nameKey: '24name13',
-		positionKey: '24name13',
+		nameKey: '24name39',
+		positionKey: '24name40',
 		team: ['Design'],
-		social: {}
-	},
-	{
-		nameKey: '24name14',
-		positionKey: '24name14',
-		team: ['Design'],
-		social: {}
-	},
+		social: {
+
+		}
+	}
+];
+
+export const pr24: TeamMember[] = [
 	{
 		nameKey: '24name15',
 		positionKey: '24name15',
-		team: ['Design'],
+		team: ['Pr'],
 		social: {
-			github: '',
 			linkedin: ''
 		}
 	},
 	{
 		nameKey: '24name16',
 		positionKey: '24name16',
-		team: ['Design'],
+		team: ['Pr'],
 		social: {
-			github: '',
-            instagram: '',
 			linkedin: ''
 		}
 	},
 	{
 		nameKey: '24name17',
 		positionKey: '24name17',
-		team: ['Design'],
+		team: ['Pr'],
 		social: {
-			instagram: '',
 			linkedin: ''
 		}
-	}
-];
-
-export const pr24: TeamMember[] = [
+	},
 	{
 		nameKey: '24name18',
 		positionKey: '24name18',
@@ -165,14 +178,119 @@ export const pr24: TeamMember[] = [
 		social: {
 			linkedin: ''
 		}
-	}
+	},
+	{
+		nameKey: '24name19',
+		positionKey: '24name19',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name20',
+		positionKey: '24name20',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name21',
+		positionKey: '24name21',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name22',
+		positionKey: '24name22',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name23',
+		positionKey: '24name24',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name24',
+		positionKey: '24name24',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name25',
+		positionKey: '24name25',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name26',
+		positionKey: '24name26',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name27',
+		positionKey: '24name27',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name28',
+		positionKey: '24name28',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name29',
+		positionKey: '24name29',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name30',
+		positionKey: '24name30',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name31',
+		positionKey: '24name31',
+		team: ['Pr'],
+		social: {
+			linkedin: ''
+		}
+	},
+	
 ];
 
 export const content24: TeamMember[] = [
 	{
-		nameKey: '24name19',
-		positionKey: '24name19',
-		team: ['leads', 'Content'],
+		nameKey: '24name32',
+		positionKey: '24name32',
+		team: ['Content'],
 		social: {
             twitter: '',
 			github: '',
@@ -180,11 +298,53 @@ export const content24: TeamMember[] = [
 		}
 	},
 	{
-		nameKey: '24name20',
-		positionKey: '24name20',
+		nameKey: '24name33',
+		positionKey: '24name33',
 		team: ['Content'],
 		social: {
-			twitter: ''
+            twitter: '',
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name34',
+		positionKey: '24name34',
+		team: ['Content'],
+		social: {
+            twitter: '',
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name36',
+		positionKey: '24name36',
+		team: ['Content'],
+		social: {
+            twitter: '',
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name37',
+		positionKey: '24name37',
+		team: ['Content'],
+		social: {
+            twitter: '',
+			github: '',
+			linkedin: ''
+		}
+	},
+	{
+		nameKey: '24name38',
+		positionKey: '24name38',
+		team: ['Content'],
+		social: {
+            twitter: '',
+			github: '',
+			linkedin: ''
 		}
 	}
 ];
